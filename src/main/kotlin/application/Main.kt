ac2f9829283dd1ced6395ee36d86914a879f1462
Isabel Siva <bell.cristina.pereiradasilva@gmail.com>
2021-11-16 15:27:41 -0300

    Realização dos exercícíos da feat/viajante

--- src/main/kotlin/application/Main.kt
+++ src/main/kotlin/application/Main.kt
@@ -1,5 +1,6 @@
 package application
-
+import io.javalin.apibuilder.ApiBuilder.*
+import model.Location
 import dao.LocationDao
 import io.javalin.Javalin
 
@@ -16,22 +17,38 @@ fun main(){
     app.routes {
 
         //TODO EXERCICIO 1
-        //insira seu código aqui
+        get("/locations") { ctx ->
+            ctx.json(locationDao.getAll())
+        }
 
         //TODO EXERCICIO 2
-        //insira seu código aqui
+        get("/locations/{location-id}") { ctx ->
+            ctx.json(locationDao.findById(ctx.pathParam("location-id").toInt())!!)
+        }
 
         /*TODO DESAFIO 1
         * Dica: Utilize essa variavel abaixo para pegar o body da requisição
-        *
-        * val localizacao = ctx.bodyAsClass<Location>()
-        * */
-
-        //TODO DESAFIO 2
-        //insira seu código aqui
+        */
+        post("/locations") { ctx ->
+            val localizacao = ctx.bodyAsClass<Location>()
+            locationDao.save(mensagem = localizacao.mensagem, longitude = localizacao.longitude, latitude = localizacao.latitude, galaxia = localizacao.galaxia, planeta = localizacao.planeta)
+            ctx.status(201)
+        }
+         //TODO DESAFIO 2
+        delete("/locations/{location-id}"){ ctx ->
+            locationDao.delete(ctx.pathParam("location-id").toInt())
+            ctx.status(204)
+        }
 
         //TODO DESAFIO 3
-        //insira seu código aqui
+        patch("/locations/{location-id}") { ctx ->
+            val localizacao = ctx.bodyAsClass<Location>()
+            locationDao.update(
+                id = ctx.pathParam("location-id").toInt(),
+                 location = localizacao
+            )
+            ctx.status(204)
+        }
 
     }
 
