fd67ff88151d0192445ca415ea152e820c36b4ab
Isabel Siva <bell.cristina.pereiradasilva@gmail.com>
2021-11-16 13:52:37 -0300

    Desfazendo a bagunça

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,18 @@
+import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder.*
+
 fun main(){
-    println("Olá Deevas!!!")
-    println("Preparadas para construirmos uma API?")
+
+//    val app = Javalin.create().start(7000)
+//    app.get("/WCC"){ctx -> ctx.result("Olá Deeevas! =D") }
+    val userDao = UserDao()
+    val app = Javalin.create().apply {
+        exception(Exception::class.java){e, ctx -> e.printStackTrace()}
+        error(404) {ctx -> ctx.json("NOT FOUND")}
+    }.start(7000)
+    //mostrando todos os usuários
+    app.routes{
+        get("/users"){ctx -> ctx.json(userDao.users)}
+        get("/users/{users-id}"){ctx -> ctx.json(userDao.findById(ctx.pathParam("user-id").toInt())!!)}
+    }
 }
