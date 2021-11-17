161d5a9699ab23c11c4365759a7b6f3a06efe9c5
Isabel Siva <bell.cristina.pereiradasilva@gmail.com>
2021-11-16 21:42:43 -0300

    refatorando o arquivo Main.kt

--- src/main/kotlin/application/Main.kt
+++ src/main/kotlin/application/Main.kt
@@ -18,12 +18,15 @@ fun main(){
 
         //TODO EXERCICIO 1
         get("/locations") { ctx ->
-            ctx.json(locationDao.getAll())
+            ctx.json(locationDao.getActualLocation()!!)
+            ctx.status(200)
         }
 
         //TODO EXERCICIO 2
-        get("/locations/{location-id}") { ctx ->
-            ctx.json(locationDao.findById(ctx.pathParam("location-id").toInt())!!)
+        get("/locations/{id}") { ctx ->
+            val id = ctx.pathParam("id").toInt()
+            ctx.json(locationDao.findById(id)!!)
+            ctx.status(200)
         }
 
         /*TODO DESAFIO 1
@@ -31,20 +34,26 @@ fun main(){
         */
         post("/locations") { ctx ->
             val localizacao = ctx.bodyAsClass<Location>()
-            locationDao.save(mensagem = localizacao.mensagem, longitude = localizacao.longitude, latitude = localizacao.latitude, galaxia = localizacao.galaxia, planeta = localizacao.planeta)
+            locationDao.save(
+                mensagem = localizacao.mensagem,
+                longitude = localizacao.longitude,
+                latitude = localizacao.latitude,
+                galaxia = localizacao.galaxia,
+                planeta = localizacao.planeta)
             ctx.status(201)
         }
          //TODO DESAFIO 2
-        delete("/locations/{location-id}"){ ctx ->
-            locationDao.delete(ctx.pathParam("location-id").toInt())
+
+        delete("/locations/{id}"){ ctx ->
+            locationDao.delete(ctx.pathParam("id").toInt())
             ctx.status(204)
         }
 
         //TODO DESAFIO 3
-        patch("/locations/{location-id}") { ctx ->
+        patch("/locations/{id}") { ctx ->
             val localizacao = ctx.bodyAsClass<Location>()
             locationDao.update(
-                id = ctx.pathParam("location-id").toInt(),
+                id = ctx.pathParam("id").toInt(),
                  location = localizacao
             )
             ctx.status(204)
